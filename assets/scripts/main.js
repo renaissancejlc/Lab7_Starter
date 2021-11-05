3146cf62a291e06184879ac4d3135c0ec2bdc7ba
renaissancejlc <91338859+renaissancejlc@users.noreply.github.com>
2021-11-04 18:45:41 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,8 @@ const router = new Router(function () {
    * This will only be two single lines
    * If you did this right, you should see the recipe cards just like last lab
    */
+  document.querySelector('section.section--recipe-cards').classList.remove('shown');
+  document.querySelector('section.section--recipe-expand').classList.add('shown');
 });
 
 window.addEventListener('DOMContentLoaded', init);
@@ -55,6 +57,7 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+
 }
 
 /**
@@ -117,6 +120,19 @@ function createRecipeCards() {
    * all the recipes. (bonus - add the class 'hidden' to every recipe card with 
    * an index greater  than 2 in your for loop to make show more button functional)
    */
+  for (let i =1; i < recipes.length(); i++){
+    router.addPage(page, function() {
+      document.querySelector('.section--recipe-cards').classList.remove('shown');
+      document.querySelector('.section--recipe-expand').classList.add('shown');
+      document.querySelector('recipe-expand').data = recipeData[recipes[i]];
+    });
+    bindRecipeCard(recipeCard, page);
+    if (i>2){
+        cards[i].classList.add('hidden');
+    }
+  
+    document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
+  }
 }
 
 /**
@@ -172,6 +188,7 @@ function bindEscKey() {
    * if the escape key is pressed, use your router to navigate() to the 'home'
    * page. This will let us go back to the home page from the detailed page.
    */
+  
 }
 
 /**
